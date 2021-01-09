29990fba06411685eeeceab108127bead0f61dff
Shaurya0802 <shauryaakhileshsharma@gmail.com>
2021-01-09 12:35:40 +0530

    C101-Project

--- uploadFiles.py
+++ uploadFiles.py
@@ -1,14 +1,14 @@
 import os
 import dropbox
 
-class TransferData(object):
+class TransferData():
     def __init__(self, access_token):
         self.access_token = access_token
 
     def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
 
-        for root, files in os.walk(file_from):
+        for root, dirs, files in os.walk(file_from):
             for name in files:
                 path = os.path.join(root, name)
                 print(path)
@@ -20,13 +20,13 @@ class TransferData(object):
                     dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode.overwrite)
 
 def main():
-    access_token = "8hXbQGIDB5IAAAAAAAAAAd-JW98XFJO8eb7LCYd-UfTtNrO76lkzAHI9HYrFof2x"
-    transfer_data = TransferData(access_token)
+    access_token = "1Ve3r3n9eEIAAAAAAAAAAZ9w0PjhwaXdodpJ0t1wm5afdnGZD2MzQsPovoKWP9n5"
+    transfer__data = TransferData(access_token)
 
     file_from = input("Enter the path of file to be taken :- ")
     file_to = input("Enter the path where the file is to be dropped :- ")
 
-    transfer_data.upload_file(file_from, file_to)
+    transfer__data.upload_file(file_from, file_to)
 
     print("Your file has been uploaded successfully")
     
